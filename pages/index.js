240f31176e8e828d697f2c76ad0d02c793eccd12
Ammar Khan <57397520+ammarkhanone@users.noreply.github.com>
2026-01-28 23:48:21 +0500

    Add pages/index.js

---
+++ pages/index.js
@@ -0,0 +1,8 @@
+export default function Home() {
+  return (
+    <main style={{ padding: '40px', textAlign: 'center' }}>
+      <h1>live-demo-app0331</h1>
+      <p>Create a hello world app</p>
+    </main>
+  )
+}
